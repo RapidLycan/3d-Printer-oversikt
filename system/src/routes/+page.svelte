7c4aefd7e1fa4a5cced9e7b444e3100a766a8e21
wolfy <halfcrazypopcorn@gmail.com>
2023-01-30 12:48:43 +0100

    made page or smthn

--- system/src/routes/+page.svelte
+++ system/src/routes/+page.svelte
@@ -1,2 +1,68 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Print noe for faen </h1>
+
+<div class="line">
+    <a href="">
+        <div class="print1 box">
+            <img src="" alt="printer 1">
+            <h1>name</h1>
+            <div>progress bar</div>
+            <p>user details</p>
+        </div>
+    </a>
+    
+    <a href="">
+        <div class="print2 box">
+            <img src="" alt="printer 2">
+            <h1>name</h1>
+            <div>progress bar</div>
+            <p>user details</p>
+        </div>
+    </a>
+</div>
+
+<div class="line">
+    <a href="">
+        <div class="print3 box">
+            <img src="" alt="printer 3">
+            <h1>name</h1>
+            <div>progress bar</div>
+            <p>user details</p>
+        </div>
+    </a>
+    
+    <a href="">
+    <div class="print4 box">
+        <img src="" alt="printer 4">
+        <h1>name</h1>
+        <div>progress bar</div>
+        <p>user details</p>
+    </div>
+</a>
+</div>
+
+
+
+<style>
+
+:global(body){
+    overflow: hidden;
+    max-width: 80rem;
+}
+
+.line{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 0 200px;
+}
+
+.box{
+    padding: 100px 100px;
+    border: 2px;
+    border-color: black;
+}
+
+</style>
+
+<script></script>
+
